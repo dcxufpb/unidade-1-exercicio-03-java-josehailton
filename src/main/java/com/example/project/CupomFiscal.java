4e3c95068865227042f00ecbfb746e4a65694743
josehailton <71135811+josehailton@users.noreply.github.com>
2020-10-03 17:04:40 -0300

    Update CupomFiscal.java
    003

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -2,7 +2,23 @@ package com.example.project;
 
 public class CupomFiscal {
 	
-	if (NOME_LOJA.isEmpty()){
+	public static String LOGRADOURO = "Av. Projetada Leste";
+	public static int NUMERO = 500;
+	public static String COMPLEMENTO = "EUC F32/33/34";
+	public static String BAIRRO = "Br. Sta Genebra";
+	public static String MUNICIPIO = "Campinas";
+	public static String ESTADO = "SP";
+	public static String CEP = "13080-395";
+	public static String TELEFONE = "(19) 3756-7408";
+	public static String OBSERVACAO = "Loja 1317 (PDP)";
+	public static String CNPJ = "42.591.651/0797-34";
+	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
+	
+	private static final String p_L = System.lineSeparator();
+	
+	public static String imprimirCupom(){
+		
+		if (NOME_LOJA.isEmpty()){
 			return "O campo nome da loja é obrigatório";
 		}
 		
@@ -74,6 +90,8 @@ public class CupomFiscal {
 		cupomPronto += String.format("IE: %s",_inscricaoEstadual) + p_L;
                 
                 return cupomPronto;
+	}
+
 
 	public static void main(final String[] args) {
 		imrpimirCupom();
